9829859ea1f56e38184f3b51d44c222afd1812d6
Ni0XXo0 <165730582+Ni0XXo0@users.noreply.github.com>
2024-06-25 16:09:54 -0300

    Update script.php

--- script.php
+++ script.php
@@ -4,25 +4,44 @@ $precototal = $_GET['precototal'];
 
 // VARIAVEL DE ACORDO COM A QUANTIDADE DE UM PASSAGEIRO
 $redirecionamentos = array(
-    138 => 'https://pay.ofertpassagem.shop/1VOvGVYkJO9ZD62',
-    286 => 'https://pay.ofertpassagem.shop/nWrxGWKkVvoG654',
-    640 => 'https://pay.ofertpassagem.shop/lDW0ZaxVr763N7E',
+    138 => 'https://pay.pag-pass.site/BNjzgP4Az4egM78',
+    286 => 'https://pay.pag-pass.site/N1nVZpK4DnL3lM6',
+    640 => 'https://pay.pag-pass.site/lDW0Zax7A0r3N7E',
     // VARIAVEL DE ACORDO COM A QUANTIDADE DE DOIS PASSAGEIROS
 	
-    276 => 'https://pay.ofertpassagem.shop/zj6aGnqWDaOZwlK',
-    572 => 'https://pay.ofertpassagem.shop/kYL6gea7VlagrKM',
-    1280 => 'https://pay.ofertpassagem.shop/eApQgzkeX943Eb7',
+    276 => 'https://pay.pag-pass.site/DPXw3XYnWq9Zzmp',
+    572 => 'https://pay.pag-pass.site/n1NLgwkLa2YgMxE',
+    1280 => 'https://pay.pag-pass.site/yOeXZKQx6lxgAQa',
     // VARIAVEL DE ACORDO COM A QUANTIDADE DE TRES PASSAGEIROS 
 	
-    414 => 'https://pay.ofertpassagem.shop/YL9jZD2Qvv6gp4q',
-    858 => 'https://pay.ofertpassagem.shop/xQBPZv08oDYgmVq',
-    1920 => 'https://pay.ofertpassagem.shop/BNjzgP46zBJgM78',
+    414 => 'https://pay.pag-pass.site/7vJOGYeYa2pGKXd',
+    858 => 'https://pay.pag-pass.site/5pjw3RVmj6m32lQ',
+    1920 => 'https://pay.pag-pass.site/JqoR32Am5LdgVj5',
     // VARIAVEL DE ACORDO COM A QUANTIDADE DE QUATRO PASSAGEIROS
 	
-    552 => 'https://pay.ofertpassagem.shop/N1nVZpKEDxe3lM6',
-    1144 => 'https://pay.ofertpassagem.shop/DPXw3XYk8KdZzmp',
-    2560 => 'https://pay.ofertpassagem.shop/ODAK3LnovMoGE6V',
+    552 => 'https://pay.pag-pass.site/4KovG1MA5pVGyDE',
+    1144 => 'https://pay.pag-pass.site/YEwR3AQ06Mp3dKy',
+    2560 => 'https://pay.pag-pass.site/rn4RgQWArknZwBV',
     // VARIAVEL DE ACORDO COM A QUANTIDADE DE CINCO PASSAGEIROS 
+
+    69 => 'https://pay.pag-pass.site/v7R0gl1dmnz3Vr9',
+    138 => 'https://pay.pag-pass.site/lqv1301LnzWZxbj',
+    207 => 'https://pay.pag-pass.site/q510ZOdvLxJGE9D',
+    // VARIAVEL DE ACORDO COM A QUANTIDADE DE TRES PASSAGEIROS
+
+    143 => 'https://pay.pag-pass.site/xQBPZv0omOagmVq',
+    286 => 'https://pay.pag-pass.site/6YQPgj5keOLGpxz',
+    429=> 'https://pay.pag-pass.site/YL9jZD2vLNLgp4q',
+    // VARIAVEL DE ACORDO COM A QUANTIDADE DE TRES PASSAGEIROS
+
+    572 => 'https://pay.pag-pass.site/nOzxZo1YmONgJ07',
+    320 => 'https://pay.pag-pass.site/v7R0gl1dm9z3Vr9',
+    640 => 'https://pay.pag-pass.site/lqv1301LneWZxbj',
+    // VARIAVEL DE ACORDO COM A QUANTIDADE DE TRES PASSAGEIROS
+
+    960 => 'https://pay.pag-pass.site/4KovG1MoNXdGyDE',
+    1280 => 'https://pay.pag-pass.site/YEwR3AQeLOP3dKy',
+    // VARIAVEL DE ACORDO COM A QUANTIDADE DE TRES PASSAGEIROS
 );
 
 // Verifica se o valor existe no array e redireciona
